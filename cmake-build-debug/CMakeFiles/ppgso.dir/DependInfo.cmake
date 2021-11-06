f927b22be69b663a33cb988d17fcb7dde1a979f4
Lings are cute <martin.svab@gmail.com>
2021-11-06 17:16:35 +0100

    fixed repository

---
+++ cmake-build-debug/CMakeFiles/ppgso.dir/DependInfo.cmake
@@ -0,0 +1,45 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/marti/Downloads/ppgso-public/ppgso-public/ppgso/image.cpp" "C:/Users/marti/Downloads/ppgso-public/ppgso-public/cmake-build-debug/CMakeFiles/ppgso.dir/ppgso/image.cpp.obj"
+  "C:/Users/marti/Downloads/ppgso-public/ppgso-public/ppgso/image_bmp.cpp" "C:/Users/marti/Downloads/ppgso-public/ppgso-public/cmake-build-debug/CMakeFiles/ppgso.dir/ppgso/image_bmp.cpp.obj"
+  "C:/Users/marti/Downloads/ppgso-public/ppgso-public/ppgso/image_raw.cpp" "C:/Users/marti/Downloads/ppgso-public/ppgso-public/cmake-build-debug/CMakeFiles/ppgso.dir/ppgso/image_raw.cpp.obj"
+  "C:/Users/marti/Downloads/ppgso-public/ppgso-public/ppgso/mesh.cpp" "C:/Users/marti/Downloads/ppgso-public/ppgso-public/cmake-build-debug/CMakeFiles/ppgso.dir/ppgso/mesh.cpp.obj"
+  "C:/Users/marti/Downloads/ppgso-public/ppgso-public/ppgso/shader.cpp" "C:/Users/marti/Downloads/ppgso-public/ppgso-public/cmake-build-debug/CMakeFiles/ppgso.dir/ppgso/shader.cpp.obj"
+  "C:/Users/marti/Downloads/ppgso-public/ppgso-public/ppgso/texture.cpp" "C:/Users/marti/Downloads/ppgso-public/ppgso-public/cmake-build-debug/CMakeFiles/ppgso.dir/ppgso/texture.cpp.obj"
+  "C:/Users/marti/Downloads/ppgso-public/ppgso-public/ppgso/tiny_obj_loader.cpp" "C:/Users/marti/Downloads/ppgso-public/ppgso-public/cmake-build-debug/CMakeFiles/ppgso.dir/ppgso/tiny_obj_loader.cpp.obj"
+  "C:/Users/marti/Downloads/ppgso-public/ppgso-public/ppgso/window.cpp" "C:/Users/marti/Downloads/ppgso-public/ppgso-public/cmake-build-debug/CMakeFiles/ppgso.dir/ppgso/window.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLEW_STATIC"
+  "GLM_FORCE_RADIANS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../ppgso"
+  "../dependencies/include"
+  "../"
+  "."
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
