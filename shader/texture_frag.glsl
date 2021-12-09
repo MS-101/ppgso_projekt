10ccd65e5a58ce129b0f4798c9037220ca969cc6
Kefalin <tankovic.lukas@yahoo.com>
2021-12-09 19:29:03 +0100

    pridany convolution

--- shader/texture_frag.glsl
+++ shader/texture_frag.glsl
@@ -16,13 +16,32 @@ out vec4 FragmentColor;
 float l;
 
 void main() {
-  // Lookup the color in Texture on coordinates given by texCoord
-  // NOTE: Texture coordinate is inverted vertically for compatibility with OBJ
-  FragmentColor = texture(Texture, vec2(texCoord.x, 1.0 - texCoord.y) + TextureOffset);
-  FragmentColor.a = Transparency;
+  //FragmentColor = texture(Texture, vec2(texCoord.x, 1.0 - texCoord.y) + TextureOffset);
+  //FragmentColor.a = Transparency;
 
-  l = 0.3*FragmentColor.r + 0.59*FragmentColor.g + 0.11*FragmentColor.b;
-  FragmentColor.r = l;
-  FragmentColor.g = l;
-  FragmentColor.b = l;
+  //greyscale
+  //l = 0.3*FragmentColor.r + 0.59*FragmentColor.g + 0.11*FragmentColor.b;
+  //FragmentColor.r = l;
+  //FragmentColor.g = l;
+  //FragmentColor.b = l;
+
+  float[25] kernel = float[] (
+  0.0,  1.0,  1.0,  1.0, 1.0,
+  -1.0,  0.0,  1.0,  1.0, 1.0,
+  -1.0, -1.0,  0.0,  1.0, 1.0,
+  -1.0, -1.0, -1.0,  0.0, 1.0,
+  -1.0, -1.0, -1.0, -1.0, 0.0);
+
+  int index = 0;
+  vec4 color = vec4(0);
+  float factor = 1.0;
+  float bias = 0.0;
+
+  for (int i = -2; i <= 2; i++) {
+    for (int j = -2; j <= 2; j++) {
+      vec2 shift = vec2(i,j) / textureSize(Texture,0);
+      color += kernel[index++] * texture(Texture, vec2(texCoord.x, 1.0 - texCoord.y) + shift);
+    }
+  }
+  FragmentColor = color / factor + vec4(bias, bias, bias, 1);
 }
